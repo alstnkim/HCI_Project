a355882e09000cacc80b2ad349e54c7438fd6fa5
alstnKim <sksklala9768@gmail.com>
2025-05-09 22:06:07 +0900

    ui 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import backgroundImg from "./component/background.png";
 import {
   AppBar,
@@ -12,11 +13,30 @@ import Home from "./pages/Home";
 import Select from "./pages/Select";
 import Main from "./pages/Main";
 import Summarization from "./pages/Summarization";
+import Summarization2 from "./pages/Summarization2";
+import ReadyForRecord from "./pages/ReadyForRecord";
+import Saved from "./pages/Saved";
+import Recording from "./pages/Recording";
+import Creating from "./pages/Creating";
+import SelectComponent from "./pages/SelectComponent";
+import HomeToolBar from "./pages/HomeToolBar";
 
 function App() {
   return (
     <div>
-      <Summarization />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/main" element={<Main />}></Route>
+          <Route path="/record" element={<ReadyForRecord />}></Route>
+          <Route path="/recording" element={<Recording />}></Route>
+          <Route path="/select" element={<Select />}></Route>
+          <Route path="/saved" element={<Saved />}></Route>
+          <Route path="/summarization" element={<Summarization />}></Route>
+          <Route path="/summarization2" element={<Summarization2 />}></Route>
+          <Route path="/creating" element={<Creating />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
